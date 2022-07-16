bfccf75ab9d8cf77997a7491cbf25565187a9b73
lmidy <lynette.midy@gmail.com>
2022-07-16 17:51:12 -0500

    Added Service Monitors Updated backend-deployment.yaml pushed to my local docker

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -2,38 +2,73 @@ from flask import Flask, render_template, request, jsonify
 
 import pymongo
 from flask_pymongo import PyMongo
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = "example-mongodb"
-app.config[
-    "MONGO_URI"
-] = "mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb"
+app.config['MONGO_DBNAME'] = 'example-mongodb'
+app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
 mongo = PyMongo(app)
+metrics = PrometheusMetrics(app, group_by='endpoint')
+metrics.info("app_info", "App Info", version="1.0.0")
+common_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by endpoints',
+    labels={'endpoint': lambda: request.endpoint}
+)
+config = Config(
+    config={
+        'sampler':
+            {
+                'type': 'const',
+                'param': 1
+            },
+        'logging': True,
+        'reporter_batch_size': 1,
+    },
+    service_name="backend")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
-
-@app.route("/")
+@app.route('/')
+@common_counter
 def homepage():
+    with jaeger_tracer.start_span('hello world') as span:
+        hw = "Hello World"
+        span.set_tag('message', "Hello World")
     return "Hello World"
 
-
-@app.route("/api")
+@app.route('/api')
+@common_counter
 def my_api():
-    answer = "something"
-    return jsonify(repsonse=answer)
+    with jaeger_tracer.start_span('api') as span:
+        answer = "something"
+        span.set_tag('message', answer)
+        return jsonify(response=answer)
 
-
-@app.route("/star", methods=["POST"])
+@app.route('/star', methods=['POST'])
+@common_counter
 def add_star():
-    star = mongo.db.stars
-    name = request.json["name"]
-    distance = request.json["distance"]
-    star_id = star.insert({"name": name, "distance": distance})
-    new_star = star.find_one({"_id": star_id})
-    output = {"name": new_star["name"], "distance": new_star["distance"]}
-    return jsonify({"result": output})
+    with jaeger_tracer.start_span('star') as span:
+        star = mongo.db.stars
+        name = request.json['name']
+        distance = request.json['distance']
+        star_id = star.insert({'name': name, 'distance': distance})
+        new_star = star.find_one({'_id': star_id})
+        output = {'name': new_star['name'], 'distance': new_star['distance']}
+        span.set_tag('status', 'star')
+        return jsonify({'result': output})
 
+# register additional default metrics
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
 if __name__ == "__main__":
     app.run()
--- reference-app/trial/docker-builder.sh
+++ reference-app/trial/docker-builder.sh
@@ -1,2 +1,2 @@
-docker build -t lynettemidy/frontend:v1.0.0 .
-docker push lynettemidy/frontend:v1.0.0
+docker build -t lynettemidy/trial:v1.0.0 .
+docker push lynettemidy/trial:v1.0.0
