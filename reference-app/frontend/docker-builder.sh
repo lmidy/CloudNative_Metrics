fabde42da443bfc92dea78fc386e84dcda33de4d
lmidy <lynette.midy@gmail.com>
2022-07-16 17:46:37 -0500

    getting tracing to work

---
+++ reference-app/frontend/docker-builder.sh
@@ -0,0 +1,2 @@
+docker build -t lynettemidy/backend:v1.0.0 .
+docker push lynettemidy/backend:v1.0.0
---
+++ reference-app/trial/docker-builder.sh
@@ -0,0 +1,2 @@
+docker build -t lynettemidy/frontend:v1.0.0 .
+docker push lynettemidy/frontend:v1.0.0
